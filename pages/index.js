7116a3f8c3d8d7b21cfc46638b21aca932cf1374
Luis <Luisf311220@gmail.com>
2023-12-03 20:51:28 -0300

    Restart_pass5.4

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import { Receita1_implementacao } from './Receita1.js';
-import { Receita2_implementacao } from './Receita2.js';
+import { Receita3_implementacao } from './Receita3.js';
 
 function Principal() {
     return (
         <div>
             <h1>Links das receitas</h1>
             <p><a href="/Receita1">Ver Receita 1</a></p>
-            <p><a href="/Receita2">Ver Receita 2</a></p>
+            <p><a href="/Receita3">Ver Receita 3</a></p>
         </div>
     )
 }
