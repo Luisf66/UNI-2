9f7eff924a10022d5e5bf32b7fcaf5571f722658
Luis <Luisf311220@gmail.com>
2023-12-04 09:59:53 -0300

    Execution1.1

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,37 @@ import { Receita6_implementacao } from './Receita6.js';
 import { Receita9_implementacao } from './[id].js';
 
 function Principal() {
+    const estiloTitulo = {
+        textAlign: 'center',
+        fontFamily: 'Arial, sans-serif' // Substitua pela fonte desejada
+    };
+
+    const estiloLista = {
+        listStyleType: 'none'
+    };
+
+    const estiloPrincipal = {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh'
+    };
+
     return (
-        <div>
-            <h1>Links das receitas</h1>
-            <p><a href="/Receita1">Ver Receita 1</a></p>
-            <p><a href="/Receita2">Ver Receita 2</a></p>
-            <p><a href="/Receita3">Ver Receita 3</a></p>
-            <p><a href="/Receita4">Ver Receita 4</a></p>
-            <p>Receita 5 Desabilitada (ERROR)</p>
-            <p><a href="/Receita6">Ver Receita 6</a></p>
-            <p><a href="/tt0095801">Ver Receita 9</a></p>
+        <div style={estiloPrincipal}>
+            <h1 style={estiloTitulo}>Links das receitas</h1>
+            <ul style={estiloLista}>
+                <li><a href="/Receita1">Ver Receita 1</a></li>
+                <li><a href="/Receita2">Ver Receita 2</a></li>
+                <li><a href="/Receita3">Ver Receita 3</a></li>
+                <li><a href="/Receita4">Ver Receita 4</a></li>
+                <li>Receita 5 Desabilitada (ERROR)</li>
+                <li><a href="/Receita6">Ver Receita 6</a></li>
+                <li><a href="/tt0095801">Ver Receita 9</a></li>
+            </ul>
         </div>
-    )
+    );
 }
 
-export default Principal
+export default Principal;
