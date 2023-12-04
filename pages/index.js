5871aa7e1887cf139988030038ef8c376ccaef0d
Luis <Luisf311220@gmail.com>
2023-12-03 21:12:45 -0300

    Restart_pass5.6

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { Receita1_implementacao } from './Receita1.js';
 import { Receita3_implementacao } from './Receita3.js';
 import { Receita4_implementacao } from './Receita4.js';
+import { Receita6_implementacao } from './Receita6.js';
 
 function Principal() {
     return (
@@ -9,6 +10,7 @@ function Principal() {
             <p><a href="/Receita1">Ver Receita 1</a></p>
             <p><a href="/Receita3">Ver Receita 3</a></p>
             <p><a href="/Receita4">Ver Receita 4</a></p>
+            <p><a href="/Receita6">Ver Receita 6</a></p>
         </div>
     )
 }
